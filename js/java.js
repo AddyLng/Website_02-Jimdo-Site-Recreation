33de78c2dcb4ddfe8ff608e93d3dd97efcfc1be8
Adrianne R. Bagadiong <bagadiongadrianner@gmail.com>
2024-06-25 16:30:30 +0800

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,25 @@
+// Owlcarousel
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        center: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            947: {
+                items: 1,
+            },
+            1000: {
+                items: 3,
+                nav: true,
+            }
+        }
+    });
+});
